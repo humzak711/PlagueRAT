a792878e2c540f5ea4f7645f8d82aba831da202f
Humza <139543666+humzak711@users.noreply.github.com>
2024-03-19 00:07:59 +0000

    Update main.py

--- PlagueRCE/main.py
+++ PlagueRCE/main.py
@@ -3,10 +3,6 @@ from modules.PlagueRCE import PlagueRCE
 # Start the server
 if __name__ == '__main__':
 
-    # Obtain host information 
-    host_ip: str = input('Enter host ip address: ') 
-    host_port: int = int(input('Enter host port: '))
-     
     # Start the server
-    PlagueRCE_server: PlagueRCE = PlagueRCE(host_ip, host_port)
+    PlagueRCE_server = PlagueRCE()
     PlagueRCE_server.start_server()
